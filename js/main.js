2e60fb33dca230f1bad231d2905168fb60cbfcb0
Aleksandr Kostiuk <kosskill2011@gmail.com>
2019-01-27 12:34:00 +0100

    new

---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+
+
+
+  var lineDrawing = anime({
+    targets: '.rope-img',
+    strokeDashoffset: [anime.setDashoffset, 0],
+    easing: 'easeInOutSine',
+    duration: 1000,
+    // direction: 'alternate',
+    loop: true,
+});
